f62c0a14db7c0b594e5ccf6781d5163335f36682
dnabar <dnabs302>
2025-03-19 03:30:20 +0000

    Added dictionary

--- src/simulateAndRecovery.py
+++ src/simulateAndRecovery.py
@@ -1,78 +1,71 @@
 import numpy as np
 import random
 import math
+from tabulate import tabulate #from ChatGpt
 
 class SimulateAndRecovery:
+    params = {}
     def get_model_parameters(self):
-        boundary_sep = random.uniform(0.5,2)
-        drift_rt = random.uniform(0.5,2)
-        nondec = random.uniform(0.1,0.5)
-        return boundary_sep, drift_rt, nondec
+        self.params['boundary_s'] = random.uniform(0.5,2)
+        self.params['drift'] = random.uniform(0.5,2)
+        self.params['nond'] = random.uniform(0.1,0.5)
 
-    def predicted_parameters(self, boundary_sep, drift_rt, nondec):
-        y = math.exp(-(boundary_sep*drift_rt))
-        r_pred = 1/(y+1)
-        m_pred = nondec + ((boundary_sep/(2*drift_rt))*((1-y)/(1+y)))
-        v_pred = (boundary_sep/(2*(math.pow(drift_rt,3))))*((1-(2*boundary_sep*drift_rt*y)-(math.pow(y,2)))/math.pow((y+1),2))
-        return r_pred, m_pred, v_pred
+    def predicted_parameters(self):
+        y = math.exp(-(self.params['boundary_s']*self.params['drift']))
+        self.params['r_pred'] = 1/(y+1)
+        self.params['m_pred'] = self.params['nond'] + ((self.params['boundary_s']/(2*self.params['drift']))*((1-y)/(1+y)))
+        self.params['v_pred'] = (self.params['boundary_s']/(2*(math.pow(self.params['drift'],3))))*((1-(2*self.params['boundary_s']*self.params['drift']*y)-(math.pow(y,2)))/math.pow((y+1),2))
 
-    def obs_parameters(self, N, r_pred, m_pred, v_pred):
-        t_obs = np.random.binomial(N, r_pred)
-        r_obs = t_obs/N
-        #m_obs = np.random.normal(m_pred, v_pred/N)
-        v_obs = np.random.gamma(shape = (N-1)/2, scale = (2*v_pred)/(N-1)) #formula partly from numpy.org, partly from slides
-        std_dev = np.sqrt(v_pred / N)
-        m_obs = np.random.normal(loc=m_pred,scale=std_dev) #formula from numpy.org
-        return t_obs, r_obs, v_obs, m_obs
+    def obs_parameters(self, N):
+        t_obs = np.random.binomial(N, self.params['r_pred'])
+        self.params['r_obs'] = t_obs/N
+        self.params['v_obs'] = np.random.gamma(shape = (N-1)/2, scale = (2*self.params['v_pred'])/(N-1)) #formula partly from numpy.org, partly from slides
+        std_dev = np.sqrt(self.params['v_pred'] / N)
+        self.params['m_obs'] = np.random.normal(loc=self.params['m_pred'],scale=std_dev) #formula from numpy.org
 
-    def inverse_eq(self, r_obs, v_obs, m_obs):
+    def inverse_eq(self):
         epsilon = 1e-8 
+        L = math.log(self.params['r_obs'] / (1 - self.params['r_obs'] + epsilon))
 
-        L = math.log(r_obs / (1 - r_obs)) if r_obs != 1 else 1  #ask prof if its okay to assume 1
-        L_sq_robs = L*math.pow(r_obs, 2)
-        L_robs = r_obs*L
-        v_est = np.sign(r_obs - 0.5)*(math.pow(L * (L_sq_robs - L_robs + r_obs - 0.5) / (v_obs),1/4))
+        L_sq_robs = L*math.pow(self.params['r_obs'], 2)
+        L_robs = self.params['r_obs']*L
 
-        a_est = L/v_est
+        if self.params['v_obs'] == 0 or np.isnan(self.params['v_obs']):
+            self.params['v_est'] = np.nan
+        else:
+            self.params['v_est'] = np.sign(self.params['r_obs'] - 0.5) * (math.pow(max((L * (L_sq_robs - L_robs + self.params['r_obs'] - 0.5) / (self.params['v_obs']+epsilon)),0), 1 / 4))
 
-        term1 = a_est/(2*v_est)
-        term2 = -(v_est)*(a_est)
-        t_est = m_obs - ((term1)*((1-math.exp(term2))/(1+math.exp(term2))))
+        self.params['a_est'] = L/(self.params['v_est'] + epsilon)
+        term1 = self.params['a_est']/(2*(self.params['v_est'] + epsilon))
+        term2 = -(self.params['v_est'])*(self.params['a_est'])
+        self.params['t_est'] = self.params['m_obs'] - ((term1)*((1-math.exp(term2))/(1+math.exp(term2))))
 
-        return v_est, a_est, t_est
 
     def n_simulations(self, N):
         bias = []
         sq_error = []
 
         for i in range(1000):
-            boundary_sep, drift_rt, nondec = self.get_model_parameters()
-            r_pred, m_pred, v_pred = self.predicted_parameters(boundary_sep, drift_rt, nondec)
-            t_obs, r_obs, v_obs, m_obs = self.obs_parameters(N, r_pred, m_pred, v_pred)
-            v_est, a_est, t_est = self.inverse_eq(r_obs, v_obs, m_obs)
+            self.get_model_parameters()
+            self.predicted_parameters()
+            self.obs_parameters(N)
+            self.inverse_eq()
 
-            bias.append(np.array([drift_rt-v_est, boundary_sep-a_est, nondec-t_est]))
+            bias.append(np.array([self.params['drift']-self.params['v_est'], self.params['boundary_s']-self.params['a_est'], self.params['nond']-self.params['t_est']]))
             sq_error.append(np.square(bias[-1]))
 
-        b_avg = np.nanmean(bias)
-        sq_error_avg = np.nanmean(sq_error) #nanmean suggested by ChatGpt
+        self.params['b_avg'] = np.nanmean(np.array(bias))
+        self.params['sq_error_avg'] = np.nanmean(np.array(sq_error)) #nanmean suggested by ChatGpt
 
-        return b_avg, sq_error_avg
 
 def main():
     obj = SimulateAndRecovery()
 
-    print("N   b   bsq")
+    data = []
     for N in [10, 40, 4000]:
-        b_avg, sq_error_avg = obj.n_simulations(N)
-        print(N, " ", round(b_avg ,1), " ", sq_error_avg)    #verify look of output
-            
+        obj.n_simulations(N)
+        data.append([N, round(obj.params['b_avg']), obj.params['sq_error_avg']])
+
+    print(tabulate(data, headers=["N", "b", "bsq"], tablefmt="grid"))     
 if __name__ == "__main__":
     main()
-
-
-    
-
-
-   
-  
--- test/test_simulateAndRecovery.py
+++ test/test_simulateAndRecovery.py
@@ -9,41 +9,41 @@ class TestSimulateAndRecovery(unittest.TestCase):
     sar = SimulateAndRecovery()
 
     def test_get_model_parameters(self):
-        boundary_sep, drift_rt, nondec = self.sar.get_model_parameters()
-        self.assertTrue(0.5 <= boundary_sep <= 2)
-        self.assertTrue(0.5 <= drift_rt <= 2)
-        self.assertTrue(0.1 <= nondec <= 0.5)
+        self.sar.get_model_parameters()
+        self.assertTrue(0.5 <= self.sar.params['boundary_s'] <= 2)
+        self.assertTrue(0.5 <= self.sar.params['drift'] <= 2)
+        self.assertTrue(0.1 <= self.sar.params['nond'] <= 0.5)
 
     def test_predicted_parameters(self):
-        boundary_sep, drift_rt, nondec = 1.6, 1.1, 0.2
-        R_pred, M_pred, V_pred = self.sar.predicted_parameters(boundary_sep, drift_rt, nondec)
-        self.assertTrue(0 <= R_pred <= 1)
-        self.assertTrue(M_pred > nondec)
-        self.assertTrue(V_pred > 0)
+        self.sar.params['boundary_s'], self.sar.params['drift'], self.sar.params['nond'] = 1.6, 1.1, 0.2
+        self.sar.predicted_parameters()
+        self.assertTrue(0 <= self.sar.params['r_pred'] <= 1)
+        self.assertTrue(self.sar.params['m_pred']> self.sar.params['nond'])
+        self.assertTrue(self.sar.params['v_pred'] > 0)
 
     def test_inverse_equations(self):
-        boundary_sep, drift_rt, nondec = 1.6, 1.1, 0.2
-        r_pred, m_pred, v_pred = self.sar.predicted_parameters(boundary_sep, drift_rt, nondec)
-        t_obs, r_obs, v_obs, m_obs = self.sar.obs_parameters(1000, r_pred, m_pred, v_pred)
-        v_est, a_est, t_est = self.sar.inverse_eq(r_obs, v_obs, m_obs)
-        self.assertAlmostEqual(boundary_sep, a_est, delta=0.2)
-        self.assertAlmostEqual(drift_rt, v_est, delta=0.2)
-        self.assertAlmostEqual(nondec, t_est, delta=0.1)
+        self.sar.params['boundary_s'], self.sar.params['drift'], self.sar.params['nond'] = 1.6, 1.1, 0.2
+        self.sar.predicted_parameters()
+        self.sar.obs_parameters(1000)
+        self.sar.inverse_eq()
+        self.assertAlmostEqual(self.sar.params['boundary_s'], self.sar.params['a_est'], delta=0.2)
+        self.assertAlmostEqual(self.sar.params['drift'], self.sar.params['v_est'], delta=0.2)
+        self.assertAlmostEqual(self.sar.params['nond'], self.sar.params['t_est'], delta=0.1)
 
     def test_b_avg_zero(self):
         for N in [10, 40, 4000]:
-            b_avg, sq_error_avg = self.sar.n_simulations(N)
-            self.assertAlmostEqual(b_avg, 0, places=0)
+            self.sar.n_simulations(N)
+            self.assertAlmostEqual(self.sar.params['b_avg'], 0, places=0)
     
     def test_sq_error_decreasing(self):
         obj = SimulateAndRecovery()
 
         prev_sq_error_avg = None
         for N in [10, 40, 4000]:
-            _, sq_error_avg = obj.n_simulations(N)
+            obj.n_simulations(N)
             if prev_sq_error_avg is not None:
-                self.assertGreaterEqual(prev_sq_error_avg, sq_error_avg)
-            prev_sq_error_avg = sq_error_avg
+                self.assertGreaterEqual(prev_sq_error_avg, self.sar.params['sq_error_avg'])
+            prev_sq_error_avg = self.sar.params['sq_error_avg']
 
 if __name__ == "__main__":
     unittest.main()
